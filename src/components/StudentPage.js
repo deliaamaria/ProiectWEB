b621622f44a5b820c90696b7ff0c8b55ec30b4c3
AnaM229 <ana.ifrim@yahoo.com>
2023-12-21 16:40:41 +0200

    add popup

--- src/components/StudentPage.js
+++ src/components/StudentPage.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import '../css/main.css';
+import '../css/student-page.css';
 
 function StudentPage() {
     useEffect(() => {
@@ -22,15 +23,36 @@ function StudentPage() {
         teacherList.innerHTML = "";
         teachers.forEach((teacher) => {
           const liElement = document.createElement('li');
-          liElement.innerHTML = teacher.name + " - " + teacher.student_number;
+
+          const spanName = document.createElement('span');
+          spanName.innerHTML = teacher.name;
+          spanName.classList.add('teacher-list-span');
+          
+          const spanNumber = document.createElement('span');
+          spanNumber.innerHTML = teacher.student_number + ' locuri disponibile';
+          spanNumber.classList.add('teacher-list-span');
+
+          liElement.appendChild(spanName);
+          liElement.appendChild(spanNumber);
 
           const sendRequestBtn = document.createElement("button");
+          sendRequestBtn.classList.add('button-30');
           sendRequestBtn.innerHTML = "Trimite Cerere";
+          sendRequestBtn.addEventListener('click', openPopup);
           liElement.appendChild(sendRequestBtn);
     
           teacherList.appendChild(liElement);
         });
       }, [])
+
+      function openPopup() {
+        document.getElementById('overlay').style.display = 'flex';
+        document.querySelector('#close-btn').addEventListener('click', closePopup);
+      }
+  
+      function closePopup() {
+        document.getElementById('overlay').style.display = 'none';
+      }
 
     return (
       <div className='content-div'>
@@ -38,6 +60,16 @@ function StudentPage() {
         <ul id='available-teacher-list'>
 
         </ul>
+
+        <div class="overlay" id="overlay">
+          <div class="popup">
+            <span class="close-btn" id='close-btn' onclick="closePopup()">X</span>
+            <h2>Cerere dizertație</h2>
+            <p>Introduceți tema dorită mai jos.</p>
+            <input type='text' placeholder='Tema'></input>
+            <button className='button-30' id='send-request-btn'>Trimite</button>
+          </div>
+        </div>
       </div>
     );
   }
